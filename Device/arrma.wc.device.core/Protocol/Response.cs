ab64d581f0db79be607cf463cf16d2081eb1791f
Станислав Данильченко <arrma99@gmail.com>
2021-09-20 00:18:29 +0300

    add reauest and response base classes

---
+++ Device/arrma.wc.device.core/Protocol/Response.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using arrma.wc.interfaces.Protocol;
+
+namespace arrma.wc.device.core.Protocol
+{
+    /// <summary>
+    /// Структура пакета ответа
+    /// </summary>
+    public struct Response : IResponse
+    {
+        public byte[] Data { get; set; }
+        public int Length { get; set; }
+        public bool Valid { get; set; }
+
+        public Response(int length)
+        {
+            Data = new byte[length];
+            Length = length;
+            Valid = false;
+        }
+
+        public override string ToString() => $"{BitConverter.ToString(Data).Replace("-", " ")} valid is {Valid}";
+    }
+}
